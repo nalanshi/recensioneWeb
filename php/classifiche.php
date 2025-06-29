91155fb099658e53a1228576b923a7934bb148dd
oniichan <ruizelin@gmail.com>
2025-06-29 18:24:46 +0200

    php fixed

--- php/classifiche.php
+++ php/classifiche.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Pagina delle classifiche
- * 
+ *
  * Questa pagina carica il template HTML delle classifiche e lo personalizza in base allo stato di login dell'utente.
- * 
+ *
  * @author DishDiveReview Team
  * @version 1.0
  */
@@ -40,11 +40,11 @@ $contenutoLogin = "";
 $headerLoginHtml = "";
 
 if (!SessionManager::isLoggedIn()) {
-  // Utente non loggato
-  $contenutoLogin = "";
+    // Utente non loggato
+    $contenutoLogin = "";
 
-  // Link di login per l'header
-  $headerLoginHtml = "<a href='login.php' class='login-link' aria-label='Login - Accedi al tuo account'>
+    // Link di login per l'header
+    $headerLoginHtml = "<a href='login.php' class='login-link' aria-label='Login - Accedi al tuo account'>
                         <div class='user-icon-bg'>
                           <svg width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' id='user-icon'>
                             <path d='M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2'></path>
@@ -54,18 +54,18 @@ if (!SessionManager::isLoggedIn()) {
                         <span class='login-text'>Login</span>
                       </a>";
 } else {
-  // Utente loggato
-  $username = $_SESSION['username'];
-  $nome = isset($_SESSION['user_data']['nome']) ? $_SESSION['user_data']['nome'] : $username;
+    // Utente loggato
+    $username = $_SESSION['username'];
+    $nome = isset($_SESSION['user_data']['nome']) ? $_SESSION['user_data']['nome'] : $username;
 
-  // Nessun contenuto per il placeholder di login
-  $contenutoLogin = "";
+    // Nessun contenuto per il placeholder di login
+    $contenutoLogin = "";
 
-  // Menu utente per l'header
-  $profilePhoto = $_SESSION['user_data']['profile_photo'] ?? $_SESSION['profile_photo'] ?? '';
-  $icon = $profilePhoto ? "<img src='../{$profilePhoto}' alt='Foto profilo di {$username}' class='user-avatar'>" : "<svg width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' id='user-icon'><path d='M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2'></path><circle cx='12' cy='7' r='4'></circle></svg>";
+    // Menu utente per l'header
+    $profilePhoto = $_SESSION['user_data']['profile_photo'] ?? $_SESSION['profile_photo'] ?? '';
+    $icon = $profilePhoto ? "<img src='../{$profilePhoto}' alt='Foto profilo di {$username}' class='user-avatar'>" : "<svg width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' id='user-icon'><path d='M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2'></path><circle cx='12' cy='7' r='4'></circle></svg>";
 
-  $headerLoginHtml = "
+    $headerLoginHtml = "
     <div class='login-link user-menu' role='button' tabindex='0' aria-haspopup='true' aria-expanded='false' aria-label='Menu utente'>
     <div class='user-icon-bg'>
       {$icon}
@@ -81,7 +81,7 @@ if (!SessionManager::isLoggedIn()) {
           <a href='logout.php'><span lang='en'>Logout</span></a>
         </div>
     </div>";
-  }
+}
 // Sostituzione dei placeholder nel template
 $DOM = str_replace("<!--LOGIN_PLACEHOLDER-->", $contenutoLogin, $DOM);
 $DOM = str_replace("<!-- HEADER_LOGIN_PLACEHOLDER -->", $headerLoginHtml, $DOM);
@@ -98,10 +98,10 @@ foreach ($topReviews as $review) {
     $title = htmlspecialchars($review['product_name']);
 
     $rowsHtml .= "<tr class='ranking-row' tabindex='0' role='link' data-review-id='{$review['review_id']}' aria-label='Dettagli {$title}'>".
-                 "<td class='rank-position' data-label='Pos.'>{$position}</td>".
-                 "<td class='product-info' data-label='Prodotto' title='Clicca per visualizzare la recensione del prodotto e i suoi commenti'><p class='product-title'>{$title}</p></td>".
-                 "<td class='rating-display' data-label='Valutazione'><div class='rating-container'><span class='rating-number'>{$ratingNum}</span><div class='rating-stars'>{$stars}</div></div></td>".
-                 "</tr>";
+        "<td class='rank-position' data-label='Pos.'>{$position}</td>".
+        "<td class='product-info' data-label='Prodotto' title='Clicca per visualizzare la recensione del prodotto e i suoi commenti'><p class='product-title'>{$title}</p></td>".
+        "<td class='rating-display' data-label='Valutazione'><div class='rating-container'><span class='rating-number'>{$ratingNum}</span><div class='rating-stars'>{$stars}</div></div></td>".
+        "</tr>";
 
     $position++;
 }
